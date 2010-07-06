20c479563f34e0be43762efacb93c1bfaf8ac76e
Chad Payne <chad@bird-dog.ca>
2010-07-06 17:22:32 -0600

    added gmap postal route search javascript

--- custom.js
+++ custom.js
@@ -391,7 +391,6 @@ jQuery(function($) {
   }
   
   	var gmapPostalSearchValue = "Enter Postal Code...";
-	
 	$('#postalSearch input.postalCode').focus(function(){ 
 		if($(this).val() == gmapPostalSearchValue) {
 			$(this).val('');
@@ -402,10 +401,10 @@ jQuery(function($) {
 		var postalCode = $('#postalSearch input.postalCode').val();
 		
 		// create gmaps url string
-		var gmapURL = "http://maps.google.ca/maps?f=d&amp;source=s_d&amp;saddr=" + postalCode + "&amp;daddr=10860+46+St+SE,+Calgary,+AB+T2C+4Y5&amp;hl=en&amp;geocode=FbR2CQMdIvky-SkbQPXtU3FxUzGFoagS-_XE-A%3BFeovCgMdI_80-SlRx94pR3pxUzE7uJHvC8SLBQ&amp;mra=ls&amp;sll=51.006302,-113.964958&amp;sspn=0.034727,0.077162&amp;ie=UTF8&amp;t=h&amp;ll=50.97561,-114.035365&amp;spn=0.04734,0.13603&amp;output=embed";
-		
+		var gmapURL = "http://maps.google.ca/maps?f=d&amp;source=s_d&amp;saddr=" + postalCode + "&amp;daddr=10860+46+St+SE,+Calgary,+Division+No.+6,+Alberta+T2C+4Y5&amp;hl=en&amp;geocode=FbR2CQMdIvky-SkbQPXtU3FxUzGFoagS-_XE-A%3BFeovCgMdI_80-SlRx94pR3pxUzE7uJHvC8SLBQ&amp;mra=ls&amp;sll=50.999274,-113.967325&amp;sspn=0.008683,0.01929&amp;g=10860+46+St+SE,+Calgary,+Division+No.+6,+Alberta+T2C+4Y5&amp;ie=UTF8&amp;ll=50.97561,-114.035365&amp;spn=0.04734,0.13603&amp;output=embed";
+
 		// create iframe
-		var gmapIframe = "<iframe id='gmapiframelarge' width='885' height='363' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='" + gmapURL + "'></iframe>";
+		var gmapIframe = "<iframe id='gmapiframelarge' width='895' height='363' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='" + gmapURL + "'></iframe>";
 		
 		//	create close large gmap iframe
 		var closeIframe = "<div class='iframeclose'></div>";
