ff2ae080484210c2fc8c963c41c0697f642b5148
Chad Payne <chad@bird-dog.ca>
2010-07-08 18:13:02 -0600

    added google maps code to custom.js

--- custom.js
+++ custom.js
@@ -437,14 +437,15 @@ jQuery(function($) {
 			  geocoder.geocode( { 'address': address}, function(results, status) {
 				if (status == google.maps.GeocoderStatus.OK) {
 			
-				  //map.setCenter(results[0].geometry.location);
+				  map.setCenter(results[0].geometry.location);
 				  
+/*		Better Centering for map
 					var bounds = new GLatLngBounds();
 					for (var i=0; i<markers.length; i++) {
 					bounds.extend(markers[i].point);
 					}
 					map.setZoom(map.getBoundsZoomLevel(bounds));
-					map.setCenter(bounds.getCenter()); 		
+					map.setCenter(bounds.getCenter()); 		*/
 					
 				  var marker = new google.maps.Marker({
 					  map: map, 
