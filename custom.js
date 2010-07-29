a64cacaa737d380717c3c12dd276a32362235152
Chad Payne <chad@bird-dog.ca>
2010-07-29 17:59:10 -0600

    Staff email window repositioned

--- custom.js
+++ custom.js
@@ -79,8 +79,8 @@ jQuery(function($) {
 
   jQuery.fn.center = function () {
     this.css("position","absolute");
-    this.css("top", ( $(window).height() - this.height() ) / 2+$(window).scrollTop() + "px");
-    this.css("left", ( $(window).width() - this.width() ) / 2+$(window).scrollLeft() + "px");
+    this.css("top", ( $(window).height() - this.height() ) / 2 + "px");
+    this.css("left", ( $(window).width() - this.width() ) / 2 + "px");
     return this;
   }
 
